cd92cfe8c5d7f1aeca519568b4205e167c92dc89
RohitKuwar <70262783+RohitKuwar@users.noreply.github.com>
2020-12-18 10:55:00 +0530

    Update typing.js

--- typing.js
+++ typing.js
@@ -6,9 +6,11 @@ const setOfWords = [
     "The Ministry of Culture plays a vital role in the preservation and promotion of art and culture. Its aim is to develop ways and means by which basic cultural and aesthetic values and perceptions remain active and dynamic among the people. It also undertakes programmes for the promotion of various manifestations of contemporary art. ",
     "Tata Motors Group is a $35 billion organisation. It is a leading global automobile manufacturing company. Its diverse portfolio includes an extensive range of cars, sports utility vehicles, trucks, buses and defence vehicles. Tata Motors is one of India's largest OEMs offering an extensive range of integrated, smart and e-mobility solutions.",
     "Machine learning is the study of computer algorithms that improve automatically through experience. It is seen as a subset of artificial intelligence. Machine learning algorithms build a model based on sample data, known as 'training data'. Machine learning algorithms are used in a wide variety of applications, such as email filtering and computer vision.",
-    "It’s time to roll out your yoga mat and discover the combination of physical and mental exercises that for thousands of years have hooked yoga practitioners around the globe. The beauty of yoga is that you don’t have to be a yogi or yogini to reap the benefits. Whether you are young or old, overweight or fit, yoga has the power to calm the mind and strengthen the body."
+    "It’s time to roll out your yoga mat and discover the combination of physical and mental exercises that for thousands of years have hooked yoga practitioners around the globe. The beauty of yoga is that you don’t have to be a yogi or yogini to reap the benefits. Whether you are young or old, overweight or fit, yoga has the power to calm the mind and strengthen the body.",
+    "An economy is an area of the production, distribution and trade, as well as consumption of goods and services by different agents. Understood in its broadest sense. The economy is defined as a social domain that emphasize the practices, discourses, and material expressions associated with the production, use, and management of resources",
+    "Plasma is the clear, straw-colored liquid portion of blood that remains after red blood cells, white blood cells, platelets and other cellular components are removed. It is the single largest component of human blood, comprising about 55 percent, and contains water, salts, enzymes, antibodies and other proteins. "
 ]
-
+ 
 const msg = document.getElementById('msg');
 const typedWords = document.getElementById('mywords');
 const btn = document.getElementById('btn');
@@ -19,14 +21,14 @@ const playGame = () => {
     msg.innerText = setOfWords[randomNumber];
     let date = new Date();
     let startTime = date.getTime();
-    btn.innerText = 'Done';
+    btn.innerText = "Done";
 }
 
 const endPlay = () => {
     let date = new Date();
     let endTime = date.getTime();
-    let startTime = date.getTime();
     let totalTime = ((endTime - startTime)/1000);
+    console.log(Number.isNaN(totalTime));
     console.log(totalTime);
 
     let totalStr = typedWords.value;
@@ -34,7 +36,7 @@ const endPlay = () => {
 
     let speed = Math.round((wordCount / totalTime) * 60);
 
-    let finalMsg = " You typed total at " +speed+ " words per minute. ";
+    let finalMsg = `Your speed of typing is ${speed} words per minutes.`;
     finalMsg += compareWords(msg.innerText, totalStr);
     msg.innerText = finalMsg;
 }
@@ -52,22 +54,24 @@ words1.forEach(function(item, index){
 })
 
 let errorWords = (words1.length - cnt);
-return (cnt + " correct out of " + words1.length + " words and the total number of  error are " + 
-        errorWords + ".");
+return(` Out of ${words1.length} words, you typed ${cnt} words correct and the total number of missing words are ${errorWords}.`);
 }
 
 const wordCounter = (str) => {
-    let response = str.split(' ').length;
+    let response = str.split(" ").length;
+    console.log(response);
     return response;
 }
 
 btn.addEventListener('click', function(){
     if(this.innerText == "Start"){
         typedWords.disabled = false;
+        msg.style.background = "green";
         playGame();
     } else if(this.innerText == "Done"){
         typedWords.disabled = true;
         btn.innerText = "Start";
+        msg.style.cssText = "background:hotpink;color:black";
         endPlay();
     }
 });
